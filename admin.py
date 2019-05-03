a3c0aa17adf32147229fb8046b683ce239257ef2
bazinga <19626297+Bazinga9000@users.noreply.github.com>
2019-05-03 19:01:20 -0400

    initial commit

---
+++ admin.py
@@ -0,0 +1,216 @@
+import discord
+from discord.ext import commands
+
+
+owner = lambda ctx: ctx.author.id in [137001076284063744,223201675186274304]
+
+class Admin():
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    #everything regarding the help command was made by bottersnike and hanss
+    def format_args(self, cmd):
+        params = list(cmd.clean_params.items())
+        p_str = ''
+        for p in params:
+            if p[1].default == p[1].empty:
+                p_str += ' <{}>'.format(p[0])
+            else:
+                p_str += ' [{}]'.format(p[0])
+
+        return p_str
+
+    def format_commands(self, prefix, cmd, name=None):
+        cmd_args = self.format_args(cmd)
+        if not name: name = cmd.name
+        name = name.replace('  ', ' ')
+        d = '`{}{}{}`\n'.format(prefix,name,cmd_args)
+
+        if type(cmd) == commands.core.Group:
+            cmds = sorted(list(cmd.commands), key=lambda x: x.name)
+            for subcmd in cmds:
+                d += self.format_commands(prefix, subcmd, name='{} {}'.format(name,subcmd.name))
+
+        return d
+
+    def get_help(self, ctx, cmd, name=None):
+        d = 'Help for command `{}`:\n'.format(cmd.name)
+        d += '\n**Usage:**\n'
+
+        d += self.format_commands(ctx.prefix, cmd, name=name)
+
+        d += '\n**Description:**\n'
+        d += '{}\n'.format('None' if cmd.brief is None else cmd.brief.strip())
+
+        if cmd.checks:
+            d += '\n**Checks:**'
+            for check in cmd.checks:
+                d += '\n{}'.format(check.__qualname__.split('.')[0])
+            d += '\n'
+
+        if cmd.aliases:
+            d += '\n**Aliases:**'
+            for alias in cmd.aliases:
+                d += '\n`{}{}`'.format(ctx.prefix,alias)
+
+            d += '\n'
+
+        return d
+
+    @commands.command()
+    async def help(self, ctx, *args):
+        '''This help message'''
+        if len(args) == 0:
+            cats = [cog for cog in self.bot.cogs]
+            cats.sort()
+            width = max([len(cat) for cat in cats]) + 2
+            d = '**Categories:**\n'
+            for cat in zip(cats[0::2], cats[1::2]):
+                d += '**`{}`**{}**`{}`**\n'.format(cat[0], ' ' * int(2.3 * (width - len(cat[0]))), cat[1])
+            if len(cats) % 2 == 1:
+                d += '**`{}`**\n'.format(cats[-1])
+
+            d += '\nUse `{0}help <category>` to list commands in a category.\n'.format(ctx.prefix)
+            d += 'Use `{0}help <command>` to get in depth help for a command.\n'.format(ctx.prefix)
+
+        elif len(args) == 1:
+            cats = {cog.lower(): cog for cog in self.bot.cogs}
+            if args[0].lower() in cats:
+                cog_name = cats[args[0].lower()]
+                d = 'Commands in category **`{}`**:\n'.format(cog_name)
+                cmds = self.bot.get_cog_commands(cog_name)
+                for cmd in sorted(list(cmds), key=lambda x: x.name):
+                    d += '\n  `{}{}`'.format(ctx.prefix, cmd.name)
+
+                    brief = cmd.brief
+                    if brief is None and cmd.help is not None:
+                        brief = cmd.help.split('\n')[0]
+
+                    if brief is not None:
+                        d += ' - {}'.format(brief)
+                d += '\n'
+            else:
+                if args[0] not in ctx.bot.all_commands:
+                    d = 'Command not found.'
+                else:
+                    cmd = ctx.bot.all_commands[args[0]]
+                    d = self.get_help(ctx, cmd)
+        else:
+            d = ''
+            cmd = ctx.bot
+            cmd_name = ''
+            for i in args:
+                i = i.replace('@', '@\u200b')
+                if cmd == ctx.bot and i in cmd.all_commands:
+                    cmd = cmd.all_commands[i]
+                    cmd_name += cmd.name + ' '
+                elif type(cmd) == commands.Group and i in cmd.all_commands:
+                    cmd = cmd.all_commands[i]
+                    cmd_name += cmd.name + ' '
+                else:
+                    if cmd == ctx.bot:
+                        d += 'Command not found.'
+                    else:
+                        d += 'No sub-command found.'.format(cmd.name, i)
+                    break
+
+            else:
+                d = self.get_help(ctx, cmd, name=cmd_name)
+
+        # d += '\n*Made by Bottersnike#3605 and hanss314#0128*'
+        return await ctx.send(d)
+
+
+    @commands.check(owner)
+    @commands.command(brief="Adds `role_2` to everyone with `role_1`")
+    async def role_add(self,ctx,role_1 : int,role_2 : int):
+        guild = ctx.guild
+        r1 = discord.utils.get(guild.roles, id=role_1)
+        r2 = discord.utils.get(guild.roles, id=role_2)
+
+        member = guild.get_member(ctx.message.author.id)
+
+        given = 0
+        members = 0
+
+        for member in guild.members:
+            #print(member.name, member.roles)
+            #print(r1,r2)
+            if r1 in member.roles:
+                print("Adding role to {}".format(member.name))
+                await member.add_roles(r2)
+                given += 1
+            members += 1
+
+        await ctx.send("Role given to {} people.".format(given))
+
+    @commands.check(owner)
+    @commands.command(brief="Removes `role_2` from everyone with `role_1`")
+    async def role_sub(self, ctx, role_1 : int,role_2 : int):
+        guild = ctx.guild
+        r1 = discord.utils.get(guild.roles, id=role_1)
+        r2 = discord.utils.get(guild.roles, id=role_2)
+
+        member = guild.get_member(ctx.message.author.id)
+
+        given = 0
+        members = 0
+
+        for member in guild.members:
+            if r1 in member.roles:
+                if r2 in member.roles:
+                    print("Removing role from {}".format(member.name))
+                    await member.remove_roles(r2)
+                    given += 1
+            members += 1
+
+        await ctx.send("Role taken from {} people.".format(given))
+
+
+    '''
+    Supervoter 564475422033838080
+Superdupervoter 564475556725260308
+Supervoter V 569333309662822410
+Supervoter IV 569333211004665859
+Superduperdupervoter 564475831653629968
+'''
+
+
+
+    @commands.check(owner)
+    @commands.command(brief="Add the next supervoter role to a person.")
+    async def supervoter(self, ctx, user_mention):
+        supervoter_hierarchy = [
+            564475422033838080,
+            564475556725260308,
+            564475831653629968,
+            569333211004665859,
+            569333309662822410
+
+        ]
+
+
+        user = ctx.message.mentions[0]
+
+        role_ids = [i.id for i in user.roles]
+
+
+        level = len(supervoter_hierarchy)
+        for n,rid in enumerate(supervoter_hierarchy):
+            if rid not in role_ids:
+                await user.add_roles(discord.utils.get(ctx.guild.roles, id=rid))
+                level = n
+                break
+
+
+        if level == len(supervoter_hierarchy):
+            return await ctx.send("<@137001076284063744> someone supervoted too many times you need to add a new role to the hierarchy")
+
+        else:
+            return await ctx.send("{} has been increased in the Supervoter Hierarchy (Now {})".format(user.name, level+1))
+
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
---
+++ main.py
@@ -0,0 +1,202 @@
+from discord.ext import commands
+import re as rgx
+import discord
+from io import TextIOWrapper, BytesIO
+import sys, traceback
+import subprocess
+import asyncio
+import concurrent
+
+owner = lambda ctx: ctx.author.id == 137001076284063744
+tracebackt = True
+
+description = '''The Board Game Bot
+Handles lots of things for Board Game TWOW
+'''
+
+# this specifies what extensions to load when the bot starts up
+startup_extensions = ["admin"]
+
+prefix = "bg!"
+
+bot = commands.Bot(command_prefix=prefix, description=description)
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+@bot.event
+async def on_ready():
+    print()
+    print(bcolors.OKGREEN + "Board Game Bot is Online!" + bcolors.ENDC)
+    print(bcolors.OKBLUE + "User: " + bot.user.name + bcolors.ENDC)
+    print(bcolors.OKBLUE + "ID: " + str(bot.user.id) + bcolors.ENDC)
+    print(bcolors.OKBLUE + "Prefix: " + bot.command_prefix + bcolors.ENDC)
+
+
+
+def gettraceback(exception):
+    if not tracebackt: return
+    lines = traceback.format_exception(type(exception), exception, exception.__traceback__)
+    print(''.join(lines))
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.NoPrivateMessage):
+        return await ctx.send("Uh oh! You friccin moron! You can't use this command in DMs!")
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send("You can't do that!")
+    elif isinstance(error, commands.errors.CommandOnCooldown):
+        return await ctx.send(
+            ctx.author.name + ", you need to put a handle on your stallions for {} more seconds. This command can only be used every {} sec!".format(
+                round(error.retry_after, 2), error.cooldown.per))
+    elif isinstance(error, commands.errors.CommandNotFound):
+        pass
+    elif isinstance(error, commands.errors.MissingRequiredArgument):
+        e = str(error)
+        arg = e.split(" ")[0]
+
+        await ctx.send("Uh oh! You friccin moron! You forgot the `" + arg + "`!")
+    elif isinstance(error, commands.UserInputError):
+        e = ' '.join(error.args)
+        error_data = rgx.findall('Converting to \"(.*)\" failed for parameter \"(.*)\"\.', e)
+        if not error_data:
+            print(bcolors.FAIL + "The command " + ctx.message.content)
+            print("(Issued by " + ctx.author.name + " in " + gname + ")")
+            print("caused " + str(error)[29:] + bcolors.ENDC)
+            await ctx.send('Error: {}'.format(' '.join(error.args)))
+            gettraceback(error)
+        else:
+            await ctx.send("Uh oh! You friccin moron! `{1}` isn't an `{0}`!".format(*error_data[0]))
+    elif isinstance(error, commands.CommandInvokeError):
+        o = error.original
+        so = str(o)
+        if isinstance(o, discord.HTTPException):
+            if "Must be 2000 or fewer in length" in so:
+                await ctx.send("Uh oh! You friccin moron! That message is too long!")
+            elif "FORBIDDEN" in so:
+                await ctx.send("Uh oh! I don't have permissions for this!")
+        else:
+            if ctx.guild is None:
+                gname = " a DM"
+            else:
+                gname = ctx.guild.name
+            print(bcolors.FAIL + "The command " + ctx.message.content)
+            print("(Issued by " + ctx.author.name + " in " + gname + ")")
+            print("caused " + str(error)[29:] + bcolors.ENDC)
+            await ctx.send("Error: {}".format(' '.join(error.args)))
+            gettraceback(error)
+    else:
+        print(bcolors.FAIL + "The command " + ctx.message.content)
+        print("(Issued by " + ctx.author.name + " in " + gname + ")")
+        print("caused " + str(error)[29:] + bcolors.ENDC)
+        await ctx.send("Error: {}".format(' '.join(error.args)))
+        gettraceback(error)
+
+
+@bot.command()
+@commands.check(owner)
+async def toggletb(ctx):
+    global tracebackt
+    if tracebackt:
+        tracebackt = False
+        await ctx.send("Traceback has been turned off!")
+    else:
+        tracebackt = True
+        await ctx.send("Traceback has been turned on!")
+
+
+@bot.command()
+@commands.check(owner)
+async def load(ctx, extension_name: str):
+    """Loads an extension."""
+
+    try:
+        bot.load_extension(extension_name)
+        await ctx.send("{} loaded.".format(extension_name))
+    except (AttributeError, ImportError) as e:
+        await ctx.send("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+
+
+@bot.command()
+@commands.check(owner)
+async def speak(ctx, channel: int, *, message: str):
+    ch = bot.get_channel(channel)
+    await ch.send(message)
+
+
+@bot.command()
+@commands.check(owner)
+async def unload(ctx, extension_name: str):
+    """Unloads an extension."""
+
+    try:
+        bot.unload_extension(extension_name)
+        await ctx.send("{} unloaded.".format(extension_name))
+    except:
+        return
+
+
+async def reload_libs(ctx):
+    excount = len(startup_extensions)
+    loaded = 0
+    for extension in startup_extensions:
+        try:
+            bot.unload_extension(extension)
+            bot.load_extension(extension)
+            loaded += 1
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print(bcolors.FAIL + 'Failed to load extension {}'.format(extension) + bcolors.ENDC + '\n{}'.format(exc))
+
+    await ctx.send(str(loaded) + "/" + str(excount) + " Extensions Reloaded.")
+
+
+@bot.command(brief="Reload all the commands")
+@commands.check(owner)
+async def reload(ctx):
+    await reload_libs(ctx)
+
+
+'''
+# Stolen from HTBote
+@bot.command(brief="Update the bot from git")
+@commands.check(owner)
+async def update(ctx):
+    process = await asyncio.create_subprocess_exec('git', 'pull', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+
+    stdout = stdout.decode().splitlines()
+    stdout = '\n'.join('+ ' + i for i in stdout)
+    stderr = stderr.decode().splitlines()
+    stderr = '\n'.join('- ' + i for i in stderr)
+
+    await ctx.author.send('`Git` response: ```diff\n{}\n{}```'.format(stdout, stderr))
+
+    await reload_libs(ctx)
+'''
+
+if __name__ == "__main__":
+
+    bot.remove_command('help')
+
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print(bcolors.FAIL + 'Failed to load extension {}'.format(extension) + bcolors.ENDC + '\n{}'.format(exc))
+
+with open("token.txt") as file:
+    token = file.read().replace("\n", "")
+
+bot.run(token)
